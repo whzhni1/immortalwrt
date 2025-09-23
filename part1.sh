f68e3c82593c8ef4ec0b4a5eabbfb10039e648d1
whzhni1 <whzhni@163.com>
2025-09-23 08:32:17 +0800

    part1.sh

--- part1.sh
+++ part1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 添加自定义 feed 源
-echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 
 # 克隆自定义软件包
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
