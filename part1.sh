de576bca7b8be4ab81c6dea5a89b2499a030951b
whzhni1 <whzhni@163.com>
2025-10-09 08:21:58 +0800

    part1.sh

--- part1.sh
+++ part1.sh
@@ -6,3 +6,4 @@
 # 克隆自定义软件包
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone --depth=1 https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
